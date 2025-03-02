c12e93d7e08e4c7a7115ab7124261ffeabb409f8
reddomeuk <academy@reddome.org>
2025-03-02 17:45:38 +0000

    Microsoft Entra ID provider consistently

--- terraform/modules/idp/outputs.tf
+++ terraform/modules/idp/outputs.tf
@@ -1,4 +1,5 @@
 # terraform/modules/idp/outputs.tf
 output "entra_idp_id" {
   value = cloudflare_zero_trust_access_identity_provider.microsoft_entra_id.id
+  description = "The ID of the Microsoft Entra ID identity provider"
 }
