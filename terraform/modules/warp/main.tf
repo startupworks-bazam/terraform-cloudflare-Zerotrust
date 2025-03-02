d068eee335c960eb7f0905bd545400595f6e8ba4
reddomeuk <academy@reddome.org>
2025-03-02 22:42:26 +0000

    Azure AD Groups

--- terraform/modules/warp/main.tf
+++ terraform/modules/warp/main.tf
@@ -83,6 +83,6 @@ resource "cloudflare_zero_trust_access_policy" "warp_enrollment_policy" {
   precedence = 1
   
   include {
-    azure_groups = ["a3008467-e39c-43f6-a7ad-4769bcefe01e", "5a071d2a-8597-4096-a6b3-1d702cfab3c4"]  # Changed from azure_group to azure_groups
+    azuread_group = ["a3008467-e39c-43f6-a7ad-4769bcefe01e", "5a071d2a-8597-4096-a6b3-1d702cfab3c4"]  # Changed from azure_group to azure_groups
   }
 }
