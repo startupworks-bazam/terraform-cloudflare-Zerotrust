8b5a7ad7b07a1a00b5dbf3a284a6c94c0f9c5be4
reddomeuk <academy@reddome.org>
2025-03-02 17:47:40 +0000

    A default gateway policy allowing all DNS traffic

--- terraform/modules/gateway/main.tf
+++ terraform/modules/gateway/main.tf
@@ -7,19 +7,21 @@ terraform {
   }
 }
 
-resource "cloudflare_teams_location" "gateway" {
+# Changed from cloudflare_teams_location to cloudflare_zero_trust_dns_location
+resource "cloudflare_zero_trust_dns_location" "gateway" {
   account_id = var.account_id
   name       = var.location_name
+  
+  # Fixed networks block syntax
   networks {
-    network = var.networks[0]  # Fix network block syntax
-    description = "Gateway Network"
+    network = var.networks[0]
   }
 }
 
 resource "cloudflare_zero_trust_gateway_policy" "gateway_policy" {
   account_id  = var.account_id
   name        = "Default Gateway Policy"
-  description = "Gateway default policy"  # Added required description
+  description = "Gateway default policy"
   precedence  = 1
   action      = "allow"
   filters     = ["dns"]
