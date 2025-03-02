cfcd05542517f6be10f885b0882179485fdc8b83
reddomeuk <academy@reddome.org>
2025-03-02 21:45:49 +0000

    ohh phewww

--- terraform/modules/device_posture/main.tf
+++ terraform/modules/device_posture/main.tf
@@ -50,40 +50,56 @@ resource "cloudflare_zero_trust_device_posture_rule" "disk_encryption" {
   }
 }
 
-# Windows Intune integration rule
-resource "cloudflare_zero_trust_device_posture_rule" "intune_integration_windows" {
-  account_id  = var.account_id
-  name        = "Microsoft Intune Compliance - Windows"
-  description = "Verify Windows device compliance with Intune policies"
-  type        = "intune"
-  
-  match {
-    platform = "windows"
-  }
-  
-  input {
-    id = cloudflare_zero_trust_device_posture_integration.intune_integration.id
-    compliance_status = "compliant"
-  }
-}
+# Windows Intune integration rule - COMMENT OUT FOR NOW
+# resource "cloudflare_zero_trust_device_posture_rule" "intune_integration_windows" {
+#   account_id  = var.account_id
+#   name        = "Microsoft Intune Compliance - Windows"
+#   description = "Verify Windows device compliance with Intune policies"
+#   type        = "intune"
+#   
+#   match {
+#     platform = "windows"
+#   }
+#   
+#   input {
+#     id = cloudflare_zero_trust_device_posture_integration.intune_integration.id
+#     compliance_status = "compliant"
+#   }
+# }
 
-# Mac Intune integration rule
-resource "cloudflare_zero_trust_device_posture_rule" "intune_integration_mac" {
-  account_id  = var.account_id
-  name        = "Microsoft Intune Compliance - Mac"
-  description = "Verify Mac device compliance with Intune policies"
-  type        = "intune"
-  
-  match {
-    platform = "mac"
-  }
-  
-  input {
-    id = cloudflare_zero_trust_device_posture_integration.intune_integration.id
-    compliance_status = "compliant"
-  }
-}
+# Mac Intune integration rule - COMMENT OUT FOR NOW
+# resource "cloudflare_zero_trust_device_posture_rule" "intune_integration_mac" {
+#   account_id  = var.account_id
+#   name        = "Microsoft Intune Compliance - Mac"
+#   description = "Verify Mac device compliance with Intune policies"
+#   type        = "intune"
+#   
+#   match {
+#     platform = "mac"
+#   }
+#   
+#   input {
+#     id = cloudflare_zero_trust_device_posture_integration.intune_integration.id
+#     compliance_status = "compliant"
+#   }
+# }
 
 resource "cloudflare_zero_trust_gateway_policy" "device_posture" {
   account_id  = var.account_id
-  name
+  name        = "Device Posture Check"
+  description = "Enforce device posture requirements"
+  precedence  = 1
+  action      = "isolate"
+  filters     = ["http", "https"]
+  
+  # Use a valid traffic expression
+  traffic     = "http.request.hostname matches '.*'"
+  
+  device_posture = jsonencode({
+    integration_ids = [
+      cloudflare_zero_trust_device_posture_rule.os_version_windows.id,
+      cloudflare_zero_trust_device_posture_rule.disk_encryption.id
+      # Removed references to commented resources
+    ]
+  })
+}
