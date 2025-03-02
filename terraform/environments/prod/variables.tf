349f6547c9b2b88b5d6d4e713e7db12c0e225663
reddomeuk <academy@reddome.org>
2025-03-02 13:59:45 +0000

    AD provider

--- terraform/environments/prod/variables.tf
+++ terraform/environments/prod/variables.tf
@@ -40,9 +40,3 @@ variable "api_token" {
   type        = string
   sensitive   = true
 }
-
-variable "azure_ad_provider_id" {
-  description = "ID of the Azure AD identity provider created in Cloudflare"
-  type        = string
-  default     = ""  # Add default value
-}
