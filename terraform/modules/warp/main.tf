7034a3de7e82f80b27112762b586ce459fa3d451
reddomeuk <academy@reddome.org>
2025-03-03 12:30:33 +0000

    File upload

--- terraform/modules/warp/main.tf
+++ terraform/modules/warp/main.tf
@@ -114,8 +114,6 @@ resource "cloudflare_zero_trust_gateway_policy" "block_file_uploads_unapproved_a
   action      = "block"
   filters     = ["http"]
   
-  # Simplify the traffic expression - this is our main issue
+  # Simple expression targeting upload endpoints
   traffic     = "http.request.uri contains \"upload\""
-  
-  # Remove identity condition for now
 }
