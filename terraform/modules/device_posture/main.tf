419a38581395388e1817d4bb005cb06f072f8f2a
reddomeuk <academy@reddome.org>
2025-03-03 15:02:49 +0000

    rd

--- terraform/modules/device_posture/main.tf
+++ terraform/modules/device_posture/main.tf
@@ -24,79 +24,81 @@ resource "cloudflare_zero_trust_device_posture_integration" "intune_integration"
 # 2. Define all the device posture rules
 # OS Version Check
 resource "cloudflare_zero_trust_device_posture_rule" "os_version_windows" {
-  account_id = var.account_id
-  name       = "Windows OS Version Check"
-  type       = "os_version"
+  account_id  = var.account_id
+  name        = "Windows OS Version Check"
   description = "Ensure Windows devices are running supported OS version"
-
-  input {
-    version = "10.0.0"
-  }
-
+  type        = "os_version"
+  
   match {
     platform = "windows"
+  }
+  
+  input {
+    version = "10.15.7"  # Windows 10
   }
 }
 
 # Disk Encryption Check
 resource "cloudflare_zero_trust_device_posture_rule" "disk_encryption" {
-  account_id = var.account_id
-  name       = "Disk Encryption Check"
-  type       = "disk_encryption"
+  account_id  = var.account_id
+  name        = "Disk Encryption Check"
   description = "Ensure device disk is encrypted"
-
+  type        = "disk_encryption"
+  
   match {
     platform = "windows"
   }
 }
 
-# Antivirus Check
-resource "cloudflare_zero_trust_device_posture_rule" "antivirus" {
-  account_id = var.account_id
-  name       = "Antivirus Running Check"
-  type       = "application"
-  description = "Ensure antivirus is running and up to date"
-
-  input {
-    running = true
-    name    = "Windows Defender"
-  }
-
-  match {
-    platform = "windows"
-  }
-}
-
-# Firewall Check
-resource "cloudflare_zero_trust_device_posture_rule" "firewall" {
-  account_id = var.account_id
-  name       = "Firewall Running Check"
-  type       = "application"
-  description = "Ensure firewall is enabled"
-
-  input {
-    running = true
-    name    = "Windows Firewall"
-  }
-
-  match {
-    platform = "windows"
-  }
-}
-
-# Conditional Intune Compliance Rule (commented out by default)
-# resource "cloudflare_zero_trust_device_posture_rule" "intune_compliance" {
-#   account_id = var.account_id
-#   name       = "Intune Compliance Check"
-#   type       = "intune"
-#   description = "Verify device compliance via Intune"
-# 
-#   input {
-#     id                  = cloudflare_zero_trust_device_posture_integration.intune_integration.id
-#     compliance_status   = "compliant"
-#   }
-# 
+# Windows Intune integration rule - COMMENT OUT FOR NOW
+# resource "cloudflare_zero_trust_device_posture_rule" "intune_integration_windows" {
+#   account_id  = var.account_id
+#   name        = "Microsoft Intune Compliance - Windows"
+#   description = "Verify Windows device compliance with Intune policies"
+#   type        = "intune"
+#   
 #   match {
 #     platform = "windows"
 #   }
+#   
+#   input {
+#     id = cloudflare_zero_trust_device_posture_integration.intune_integration.id
+#     compliance_status = "compliant"
+#   }
+# }
+
+# Mac Intune integration rule - COMMENT OUT FOR NOW
+# resource "cloudflare_zero_trust_device_posture_rule" "intune_integration_mac" {
+#   account_id  = var.account_id
+#   name        = "Microsoft Intune Compliance - Mac"
+#   description = "Verify Mac device compliance with Intune policies"
+#   type        = "intune"
+#   
+#   match {
+#     platform = "mac"
+#   }
+#   
+#   input {
+#     id = cloudflare_zero_trust_device_posture_integration.intune_integration.id
+#     compliance_status = "compliant"
+#   }
+# }
+
+# Comment out the device_posture policy
+# resource "cloudflare_zero_trust_gateway_policy" "device_posture" {
+#   account_id  = var.account_id
+#   name        = "Device Posture Check"
+#   description = "Enforce device posture requirements"
+#   precedence  = 1
+#   action      = "isolate"
+#   filters     = ["http", "https"]
+#   
+#   traffic     = "http.request.hostname matches '.*'"
+#   
+#   device_posture = jsonencode({
+#     integration_ids = [
+#       cloudflare_zero_trust_device_posture_rule.os_version_windows.id,
+#       cloudflare_zero_trust_device_posture_rule.disk_encryption.id
+#     ]
+#   })
 # }
