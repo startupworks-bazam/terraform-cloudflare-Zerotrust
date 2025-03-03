68b832df89e8702587ca3a2c4ea224ab9981a598
reddomeuk <academy@reddome.org>
2025-03-03 15:25:36 +0000

    Change-

--- terraform/modules/device_posture/main.tf
+++ terraform/modules/device_posture/main.tf
@@ -23,82 +23,46 @@ resource "cloudflare_zero_trust_device_posture_integration" "intune_integration"
 
 # 2. Define all the device posture rules
 # OS Version Check
-#resource "cloudflare_zero_trust_device_posture_rule" "os_version_windows" {
-#  account_id  = var.account_id
-# name        = "Windows OS Version Check"
-#  description = "Ensure Windows devices are running supported OS version"
-#  type        = "os_version"
-#  
-#  match {
-#    platform = "windows"
-#  }
-#  
-#  input {
-#    version = "10.15.7"  # Windows 10
-#  }
-#}
+resource "cloudflare_zero_trust_device_posture_rule" "os_version_windows" {
+  account_id = var.account_id
+  name       = "Windows OS Version Check"
+  type       = "os_version"
+  description = "Ensure Windows devices are running supported OS version"
+
+  input {
+    version = "10.0.19041"  # Windows 10 version 2004 (May 2020 Update)
+  }
+
+  match {
+    platform = "windows"
+  }
+}
 
 # Disk Encryption Check
 resource "cloudflare_zero_trust_device_posture_rule" "disk_encryption" {
-  account_id  = var.account_id
-  name        = "Disk Encryption Check"
+  account_id = var.account_id
+  name       = "Disk Encryption Check"
+  type       = "disk_encryption"
   description = "Ensure device disk is encrypted"
-  type        = "disk_encryption"
   
   match {
     platform = "windows"
   }
 }
 
-# Windows Intune integration rule - COMMENT OUT FOR NOW
-# resource "cloudflare_zero_trust_device_posture_rule" "intune_integration_windows" {
-#   account_id  = var.account_id
-#   name        = "Microsoft Intune Compliance - Windows"
-#   description = "Verify Windows device compliance with Intune policies"
-#   type        = "intune"
-#   
-#   match {
-#     platform = "windows"
-#   }
-#   
-#   input {
-#     id = cloudflare_zero_trust_device_posture_integration.intune_integration.id
-#     compliance_status = "compliant"
-#   }
-# }
+# Optional Intune Compliance Rule
+resource "cloudflare_zero_trust_device_posture_rule" "intune_compliance" {
+  account_id = var.account_id
+  name       = "Intune Compliance Check"
+  type       = "intune"
+  description = "Verify device compliance via Intune"
 
-# Mac Intune integration rule - COMMENT OUT FOR NOW
-# resource "cloudflare_zero_trust_device_posture_rule" "intune_integration_mac" {
-#   account_id  = var.account_id
-#   name        = "Microsoft Intune Compliance - Mac"
-#   description = "Verify Mac device compliance with Intune policies"
-#   type        = "intune"
-#   
-#   match {
-#     platform = "mac"
-#   }
-#   
-#   input {
-#     id = cloudflare_zero_trust_device_posture_integration.intune_integration.id
-#     compliance_status = "compliant"
-#   }
-# }
+  input {
+    id                = cloudflare_zero_trust_device_posture_integration.intune_integration.id
+    compliance_status = "compliant"
+  }
 
-# Comment out the device_posture policy
-# resource "cloudflare_zero_trust_gateway_policy" "device_posture" {
-#   account_id  = var.account_id
-#   name        = "Device Posture Check"
-#   description = "Enforce device posture requirements"
-#   precedence  = 1
-#   action      = "isolate"
-#   filters     = ["http", "https"]
-#   
-#   traffic     = "http.request.hostname matches '.*'"
-#   
-#   device_posture = jsonencode({
-#     integration_ids = [
-#       cloudflare_zero_trust_device_posture_rule.os_version_windows.id,
-#       cloudflare_zero_trust_device_posture_rule.disk_encryption.id
-#     ]
-#   })
-# }
+  match {
+    platform = "windows"
+  }
+}
